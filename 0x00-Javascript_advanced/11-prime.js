8e0c3d23cab5875a555fec94339528395eab603e
Adam Taylor <tayloradam1999@gmail.com>
2021-09-22 21:32:27 +0000

    Pushing attempt at task 11

---
+++ 0x00-Javascript_advanced/11-prime.js
@@ -0,0 +1,23 @@
+setTimeout(() => {console.log(`Execution time of priting countPrimerNumbers was ${t1 - t0} milliseconds.`)}, 0);
+
+function countPrimeNumbers() {
+	let primes = [];
+	for (var counter = 2; counter <= 100; counter++) {
+    	var not_prime = false;
+    	for (var div = 3; div <= (counter - 1); div++) {
+			if (counter % div === 0) {
+				not_prime = true;
+			}
+    	}
+    	if (not_prime === false) {
+			primes.push(counter);
+		}
+	}
+	return primes;
+}
+
+const t0 = performance.now();
+for (let i = 0; i <= 100; i++) {
+	countPrimeNumbers();
+}
+const t1 = performance.now();
