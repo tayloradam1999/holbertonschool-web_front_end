fbbe4930cf808f30433a3bd936ca464266097049
Adam Taylor <tayloradam1999@gmail.com>
2021-09-22 21:10:55 +0000

    Fix performance.now

--- 0x00-Javascript_advanced/9-prime.js
+++ 0x00-Javascript_advanced/9-prime.js
@@ -13,5 +13,7 @@ function countPrimeNumbers() {
 	}
 }
 
-let x = performance.now();
-console.log("Execution time of printing countPrimeNumbers was " + x + " milliseconds");
+const t0 = performance.now();
+countPrimeNumbers();
+const t1 = performance.now();
+console.log(`Execution time of priting countPrimerNumbers was ${t1 - t0} milliseconds.`);
