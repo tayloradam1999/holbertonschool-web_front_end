17e81b92e009329d1e177ff241f36ef08d9f616e
Adam Taylor <tayloradam1999@gmail.com>
2021-09-23 00:50:50 +0000

    Please tell me why tasks 5, 6 and 14 are even in this project

---
+++ 0x00-Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,15 @@
+function createElement(data) {
+	var pTag = document.createElement("p");
+	pTag.innerHTML(data);
+	document.body.appendChild(pTag);
+}
+
+function queryWikipedia(callback) {
+	var myReq = new XMLHttpRequest();
+	myReq.addEventListener("load", createElement);
+	myReq.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*");
+	myReq.send();
+	callback(myReq.response);
+}
+
+queryWikipedia(createElement);
